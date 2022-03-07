820a10581f4256ff64e07b0e74d96ca092bb76a2
moroishi <ryuta.moroishi@is-tech.co.jp>
2022-03-07 12:55:04 +0900

    計算機プログラム(2022.3.7)

---
+++ src/Calc.java
@@ -0,0 +1,14 @@
+public class Calc {
+    public static double add(double a, double b) {
+        return (a + b);
+    }
+    public static double sub(double a, double b) {
+        return (a - b);
+    }
+    public static double multi(double a, double b) {
+        return (a * b);
+    }
+    public static double div(double a, double b) {
+        return (a / b);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("式を入力してください");
+        double inputNum1 = new java.util.Scanner(System.in).nextDouble();
+        String inputOpe = new java.util.Scanner(System.in).nextLine();
+        double inputNum2 = new java.util.Scanner(System.in).nextDouble();
+
+        double result = 0;
+        switch (inputOpe) {
+            case "+":
+                result = Calc.add(inputNum1, inputNum2);
+                break;
+            case "-":
+                result = Calc.sub(inputNum1, inputNum2);
+                break;
+            case "*":
+                result = Calc.multi(inputNum1, inputNum1);
+                break;
+            case "/":
+                result = Calc.div(inputNum1, inputNum2);
+                break;
+        }
+
+        System.out.println("計算結果：" + result);
+    }
 }
