39a2538e3daf1295ee907bb595f1f0e6fc234f5d
moroishi <ryuta.moroishi@is-tech.co.jp>
2022-03-16 12:09:59 +0900

    2022.3.16

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("式を入力してください");
-        double inputNum1 = new java.util.Scanner(System.in).nextDouble();
-        String inputOpe = new java.util.Scanner(System.in).nextLine();
-        double inputNum2 = new java.util.Scanner(System.in).nextDouble();
+        int i = 0;
+        while(i < 1){
+            System.out.println("式を入力してください");
+            double inputNum1 = new java.util.Scanner(System.in).nextDouble();
+            String inputOpe = new java.util.Scanner(System.in).nextLine();
+            double inputNum2 = new java.util.Scanner(System.in).nextDouble();
 
-        double result = 0;
-        switch (inputOpe) {
-            case "+":
-                result = Calc.add(inputNum1, inputNum2);
-                break;
-            case "-":
-                result = Calc.sub(inputNum1, inputNum2);
-                break;
-            case "*":
-                result = Calc.multi(inputNum1, inputNum1);
-                break;
-            case "/":
-                result = Calc.div(inputNum1, inputNum2);
-                break;
+            double result = 0;
+            switch (inputOpe) {
+                case "+":
+                    result = Calc.add(inputNum1, inputNum2);
+                    break;
+                case "-":
+                    result = Calc.sub(inputNum1, inputNum2);
+                    break;
+                case "*":
+                    result = Calc.multi(inputNum1, inputNum2);
+                    break;
+                case "/":
+                    result = Calc.div(inputNum1, inputNum2);
+                    break;
+            }
+
+            System.out.println("計算結果：" + Format.format(result));
         }
-
-        System.out.println("計算結果：" + result);
     }
 }
